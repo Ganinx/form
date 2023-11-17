887dfba50cc6bd99c3a9f7c44f7dbda37a67917e
valentin <delomenie-v-87@hotmail.fr>
2023-11-17 22:17:00 +0100

    formulaire qui fonctionne

--- inscription.php
+++ inscription.php
@@ -10,29 +10,57 @@
     include'block/stylesheet.php';
     ?>
     <?php
+    $pattern = "/^(?=.*[!@#$%^&*()_+={}\[\]:;<>,.?\/\-])(?=.*[0-9])(?=.*[A-Z]).{8,}$/";
     $errors = [];
     if($_SERVER["REQUEST_METHOD"]=="POST"){
-        if(empty($_POST["prenom"])){
-            $errors["prenom"] = "veuillez saisir un identifiant";
-        };
         if(strlen($_POST["prenom"])< 3){
             $errors['prenom'] = "pas assez de lettres";
         }
+        if(empty($_POST["prenom"])){
+            $errors["prenom"] = "veuillez saisir un identifiant";
+        }
     }
     if($_SERVER["REQUEST_METHOD"]=="POST"){
-        if(empty($_POST["nom"])){
-            $errors["nom"] = "veuillez saisir un identifiant";
-        };
         if(strlen ($_POST["nom"])< 3){
             $errors['nom'] = "pas assez de lettres";
+        }
+        if(empty($_POST["nom"])){
+            $errors["nom"] = "veuillez saisir un identifiant";
         }
     }
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         if(empty($_POST["mail"])){
             $errors["mail"] = "veuillez saisir un identifiant";
-        };
+        }
         if(strlen($_POST["mail"])< 3){
             $errors['mail'] = "pas assez de lettres";
+        }
+    }
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        if(empty($_POST["creaPswd"])){
+            $errors["creaPswd"] = "veuillez saisir un identifiant";
+        }
+        if(strlen($_POST["creaPswd"])< 3){
+            $errors['creaPswd'] = "pas assez de lettres";
+        }
+        if(!preg_match($pattern,$_POST['creaPswd'])) {
+          $errors['creaPswd'] = "pas bon la aussi";
+        }
+        if(str_contains($_POST['creaPswd'],$_POST['prenom']) || str_contains($_POST['creaPswd'],$_POST['nom'])){
+        $errors['creaPswd'] = "pas bon la";
+
+        }
+
+    }
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        if(empty($_POST["pswd"])){
+            $errors["pswd"] = "veuillez saisir un identifiant";
+        }
+        if(strlen($_POST["pswd"])< 3){
+            $errors['pswd'] = "pas assez de lettres";
+        }
+        if($_POST["creaPswd"] !=$_POST["pswd"] ){
+            $errors["pswd"] = "veuillez saisir le meme mot de passe";
         }
     }
     ?>
@@ -92,19 +120,51 @@
             echo('is-invalid');
         }
         ?>" name="mail">
-        <div>
+        <div class="invalid-feedback">
             <?php
-            if (filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
-            echo ("L'adresse email ".$_POST["mail"]." est considérée comme valide.");
+            if(filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
             } else {
             echo ("L'adresse email ".$_POST["mail"]." est considérée comme invalide.");
             }
             ?>
         </div>
         <label for="creaPswd">Creer votre mot de passe</label>
-        <input type="password" name="creaPswd">
+        <input type="password" value="<?php
+        if(!empty($_POST['creaPswd'])){
+            echo($_POST['creaPswd']);
+        }
+
+        ?>" class="form-control <?php
+        if(array_key_exists("creaPswd",$errors)){
+            echo('is-invalid');
+        }
+        ?>" name="creaPswd">
+        <div class="invalid-feedback">
+            <?php
+            if(array_key_exists("creaPswd",$errors)){
+                echo($errors["creaPswd"]);
+            }
+            ?>
+
+        </div>
         <label for="pswd">Confirmation</label>
-        <input type="password" name="pswd">
+        <input type="password" value="<?php
+        if(!empty($_POST['pswd'])){
+            echo($_POST['pswd']);
+        }
+
+        ?>" class="form-control <?php
+        if(array_key_exists("pswd",$errors)){
+            echo('is-invalid');
+        }
+        ?>" name="pswd">
+        <div class="invalid-feedback">
+            <?php
+            if(array_key_exists("pswd",$errors)){
+                echo($errors["pswd"]);
+            }
+            ?>
+        </div>
         <input type="submit" name="verif">
     </form>
     </div>
